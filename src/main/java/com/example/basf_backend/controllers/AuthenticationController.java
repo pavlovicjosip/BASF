daea95a7f624289744939a918347045bbcc08a91
Josip Pavlovic <josip.pavlovic@aktivis.hr>
2024-08-30 13:02:23 +0200

    remove unnecessary import

--- src/main/java/com/example/basf_backend/controllers/AuthenticationController.java
+++ src/main/java/com/example/basf_backend/controllers/AuthenticationController.java
@@ -5,12 +5,14 @@ import com.example.basf_backend.models.AuthRequest;
 import com.example.basf_backend.models.entities.UserInfo;
 import com.example.basf_backend.services.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/auth")
 @RestController
